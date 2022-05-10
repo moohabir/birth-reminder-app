d9a5f1bfb744de5b8a2e3b8e1871e7bf43caf573
moohabir <87937848+moohabir@users.noreply.github.com>
2022-05-10 21:17:52 +0200

    show button added to return back data oclick worked while taking data for setOpen but error whan open is used for setOpen.

--- src/Birth.js
+++ src/Birth.js
@@ -1,25 +1,27 @@
 import { useState } from "react";
-import data from "./data"
+import data from "./data";
 
 export default function Birth() {
-  const [open, setOpen]= useState(data)
+  const [open, setOpen] = useState(data);
+  const [show, setShow] = useState(false);
 
   return (
     <div className="birth">
       <h3>{open.length} peoole have Birthday Today</h3>
       {open.map((item) => {
-        return <div key={item.id} {...item}>
-          <div>
-            <h3>{item.name}</h3>
-            <p>{item.age}</p>
+        return (
+          <div key={item.id} {...item}>
+            <div>
+              <h3>{item.name}</h3>
+              <p>{item.age}</p>
+            </div>
+            <div></div>
           </div>
-          <div>
-          </div>
-        </div>
-     
+        );
       })}
       <div>
-        <button onClick={() => setOpen([])} >Set</button>
+        <button onClick={() => setOpen([])}>Set</button>
+        <button onClick={() => setOpen(data)}>show</button>
       </div>
     </div>
   );
